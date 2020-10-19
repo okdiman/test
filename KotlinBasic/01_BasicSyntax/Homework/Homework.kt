67205dac862ef28da7bf4a83fe05a8eb08279e11
Okunev Dmitrii <okdiman@rambler.ru>
2020-10-19 20:14:21 +0800

    change gitignore

---
+++ KotlinBasic/01_BasicSyntax/Homework/Homework.kt
@@ -0,0 +1,24 @@
+package com.skillbox.skillbox.kotlin
+
+fun main() {
+
+    val firstname = "Dmitrii"
+    val lastName = "Okunev"
+    var height = 178
+    val weight = 78f
+    var isChild: Boolean = height <= 150 || weight <= 40
+    var info = "I am $firstname $lastName, my height is $height and weight is $weight and ${if (isChild) "i am a child" else "i am not a child" }"
+    println(info)
+    height = 135
+    isChild= height <= 150 || weight <= 40
+    info = "I am $firstname $lastName, my height is $height and weight is $weight and ${if (isChild) "i am a child" else "i am not a child" }"
+    println(info)
+
+
+}
+
+
+
+
+
+
