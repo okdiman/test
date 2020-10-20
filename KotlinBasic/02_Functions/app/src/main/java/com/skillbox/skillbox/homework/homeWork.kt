3ad8f7a3a4693dc4a2a1b2f79995b1c6f2826281
Okunev Dmitrii <okdiman@rambler.ru>
2020-10-20 21:44:10 +0800

    homework

--- KotlinBasic/02_Functions/app/src/main/java/com/skillbox/skillbox/homework/homeWork.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/skillbox/skillbox/homework/homeWork.kt
@@ -3,7 +3,7 @@ package com.skillbox.skillbox.homework
 import kotlin.math.sqrt
 
 fun main(){
-    val solutionSum = solveEquation (a = 3,b = 5,c = 1)
+    val solutionSum = solveEquation (a = 4,b = 6,c = 1)
     println(solutionSum)
 }
 
@@ -11,8 +11,8 @@ fun solveEquation (a: Int,b: Int,c: Int): Any {
     //расчет дискриминанта
     val d: Int = (b * b - 4 * a * c)
     //расчет корней
-    val x1 = (-b + sqrt(d.toDouble()) / (2 * a))
-    val x2 = (-b - sqrt(d.toDouble()) / (2 * a))
+    val x1 = (-b + sqrt(d.toDouble())) / (2 * a)
+    val x2 = (-b - sqrt(d.toDouble())) / (2 * a)
     println(x1)
     println(x2)
     return if (d > 0) x1 + x2 else "no solutions"
