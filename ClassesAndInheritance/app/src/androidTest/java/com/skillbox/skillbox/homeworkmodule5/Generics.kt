1b6387a9a94f080a13c5d9c163a9321b22eb937d
Okunev Dmitrii <okdiman@rambler.ru>
2020-12-15 22:59:45 +0500

    123

---
+++ ClassesAndInheritance/app/src/androidTest/java/com/skillbox/skillbox/homeworkmodule5/Generics.kt
@@ -0,0 +1,43 @@
+package com.skillbox.skillbox.homeworkmodule5
+
+fun main() {
+    val genericObject = GenericClass(1.1)
+
+    val genericObject2 = GenericClass(1.1)
+    val genericObject3 = GenericClass(1f)
+    val genericObject4 = GenericClass(1f)
+
+    println(sumGeneric(genericObject,genericObject2))
+    println(sumGeneric(genericObject3,genericObject4))
+
+    updateContrvariantClass(ContrvariantClass<Any>(465))
+
+}
+
+class GenericClass<out T: Number> (default: T) {
+    private var item: T = default
+
+    fun getItem(): T{
+        return item
+    }
+}
+
+class ContrvariantClass <in T> (default: T){
+    private var item: T = default
+
+    fun setItem(newItem: T){
+        item = newItem
+    }
+}
+
+fun updateContrvariantClass (input: ContrvariantClass<Number>){
+    input.setItem(2.2)
+}
+
+fun sumGeneric (a:GenericClass<Number>, b: GenericClass<Number>): Int{
+    return a.getItem().toInt() + b.getItem().toInt()
+}
+
+fun <T> printGenericObject (item : T){
+    println(item.toString())
+}
