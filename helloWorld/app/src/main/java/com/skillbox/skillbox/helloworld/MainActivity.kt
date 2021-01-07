c480de5c64e5c7b8a4ca4750c30edf575b9afcac
Okunev Dmitrii <okdiman@rambler.ru>
2021-01-08 02:18:57 +0500

    123

---
+++ helloWorld/app/src/main/java/com/skillbox/skillbox/helloworld/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.skillbox.skillbox.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = """
+           Build Type = ${BuildConfig.BUILD_TYPE} 
+           Flavor = ${BuildConfig.FLAVOR}
+           Version Code = ${BuildConfig.VERSION_CODE}
+           Version Name = ${BuildConfig.VERSION_NAME}
+        """
+    }
+}
