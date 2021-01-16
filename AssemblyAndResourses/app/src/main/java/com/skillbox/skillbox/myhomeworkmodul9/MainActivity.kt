bd61a1a3ef1e0c8cd3e3e468f7d538b4470919bf
Okunev Dmitrii <okdiman@rambler.ru>
2021-01-16 20:29:32 +0500

    hey

--- AssemblyAndResourses/app/src/main/java/com/skillbox/skillbox/myhomeworkmodul9/MainActivity.kt
+++ AssemblyAndResourses/app/src/main/java/com/skillbox/skillbox/myhomeworkmodul9/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.skillbox.skillbox.myhomeworkmodul9
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clearButton.setOnClickListener {
+            nameInput.setText("")
+            Toast.makeText(this, R.string.cleared_text, Toast.LENGTH_SHORT).show()
+        }
+
+        nameInput.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                nameText.text = s?.takeIf { it.isNotBlank() }
+                        ?.let { name -> resources.getString(R.string.hello_string, name) }
+                clearButton.isEnabled = s?.isNotEmpty() ?: false
+
+            }
+        })
+        makeLongOperationButton.setOnClickListener {
+            makeLongOperation()
+        }
 //        val textView = findViewById<TextView>(R.id.textView)
 //        textView.text = """
 //            Flavor = ${BuildConfig.FLAVOR}
@@ -17,5 +43,15 @@ class MainActivity : AppCompatActivity() {
 //            Version Name = ${BuildConfig.VERSION_NAME}
 //            Application Id = ${BuildConfig.APPLICATION_ID}
 //        """.trimIndent()
+    }
+    private fun makeLongOperation () {
+        longOperationProgress.visibility = View.VISIBLE
+        makeLongOperationButton.isEnabled = false
+
+        Handler().postDelayed({
+            longOperationProgress.visibility = View.GONE
+            makeLongOperationButton.isEnabled = true
+            Toast.makeText(this,R.string.long_operation_coplete, Toast.LENGTH_SHORT).show()
+        }, 2000)
     }
 }
