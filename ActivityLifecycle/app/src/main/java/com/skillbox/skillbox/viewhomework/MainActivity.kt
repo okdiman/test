da8693ac2d3ebb36548829171b397b6b2e988f83
Okunev Dmitrii <okdiman@rambler.ru>
2021-03-10 19:48:40 +0500

    go

--- ActivityLifecycle/app/src/main/java/com/skillbox/skillbox/viewhomework/MainActivity.kt
+++ ActivityLifecycle/app/src/main/java/com/skillbox/skillbox/viewhomework/MainActivity.kt
@@ -1,47 +1,75 @@
 package com.skillbox.skillbox.viewhomework
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val tag = "MainActivity"
+    private var uncorrectlyState: FormState = FormState(false, "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.v(tag, "onCreated ${hashCode()}")
+
         var e_mailIsNotEmpty = false
         var passwordIsNotEmpty = false
         var agreementCompleted = false
+        var successLogin = false
+        val newProgressBar = layoutInflater.inflate(R.layout.loader, mainConstraint, false)
 
-        fun activateButton() {
-            if (e_mailIsNotEmpty && passwordIsNotEmpty && agreementCompleted) {
-                entrance.isEnabled = true
+        fun assessment() {
+            successLogin = e_mailIsNotEmpty && passwordIsNotEmpty && agreementCompleted
+        }
+
+        fun checkIn() {
+            if (successLogin) {
+                mainConstraint.addView(newProgressBar)
+                newProgressBar.apply {
+                    login()
+                }
+                android.os.Handler().postDelayed({
+                    mainConstraint.removeView(newProgressBar)
+                }, 2500)
+            } else {
+             uncorrectly.isVisible = true
             }
         }
 
         agreement.setOnCheckedChangeListener { buttonView, isChecked ->
+            uncorrectly.isVisible = false
             if (isChecked) {
                 agreementCompleted = true
-                activateButton()
-            } else{
-                entrance.isEnabled = false
+                assessment()
+            } else {
+                successLogin = false
             }
         }
 
-
         e_mail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                e_mailIsNotEmpty = e_mail.text.isNotEmpty()?:false
+                uncorrectly.isVisible = false
+                e_mailIsNotEmpty = e_mail.text.isNotEmpty() ?: false
                 if (e_mailIsNotEmpty) {
-                    activateButton()
-                } else{
-                    entrance.isEnabled = false
+                    assessment()
+                } else {
+                    successLogin = false
                 }
             }
 
@@ -49,47 +77,88 @@ class MainActivity : AppCompatActivity() {
         })
 
         password_e_mail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                passwordIsNotEmpty = password_e_mail.text.isNotEmpty()?:false
+                uncorrectly.isVisible = false
+                passwordIsNotEmpty = password_e_mail.text.isNotEmpty() ?: false
                 if (passwordIsNotEmpty) {
-                    activateButton()
-                }else{
-                        entrance.isEnabled = false
-                    }
+                    assessment()
+                } else {
+                    successLogin = false
                 }
-
-
+            }
 
             override fun afterTextChanged(s: Editable?) {}
         })
 
-
-        val newProgressBar = layoutInflater.inflate(R.layout.loader, mainConstraint, false)
         entrance.setOnClickListener {
-            mainConstraint.addView(newProgressBar)
-            newProgressBar.apply {
-                login()
-            }
-            android.os.Handler().postDelayed({
-                mainConstraint.removeView(newProgressBar)
-            }, 2500)
+            checkIn()
         }
+
+        ANR.setOnClickListener {
+            Thread.sleep(8000)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "onStarted ${hashCode()}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "onResumed ${hashCode()}")
+    }
+
+    override fun onTopResumedActivityChanged(isTopResumedActivity: Boolean) {
+        super.onTopResumedActivityChanged(isTopResumedActivity)
+        Log.e(tag, "onTopResumedActivityChanged ${hashCode()}")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(tag, "onPaused ${hashCode()}")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(tag, "onStopped ${hashCode()}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "onDestroyed ${hashCode()}")
     }
 
     private fun login() {
         entrance.isEnabled = false
+        entrance.text = "Введен логин и пароль"
         e_mail.isEnabled = false
         password_e_mail.isEnabled = false
         agreement.isEnabled = false
 
         android.os.Handler().postDelayed({
             entrance.isEnabled = true
+            entrance.text = "Вход"
             e_mail.isEnabled = true
             password_e_mail.isEnabled = true
             agreement.isEnabled = true
             Toast.makeText(this, "Вы успешно вошли!", Toast.LENGTH_SHORT).show()
         }, 2500)
+
+    }
+    companion object {
+        private var STATE_KEY = "stateKey"
     }
 }
