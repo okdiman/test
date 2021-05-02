41f9e4b94d0229ab0c803c73d563da011e908f79
Okunev Dmitrii <okdiman@rambler.ru>
2021-05-02 18:33:44 +0500

    go

---
+++ ConstraintLayoutAndToolbars/viewPager/app/src/main/java/com/skillbox/skillbox/viewpager/OnboardingAdapter.kt
@@ -0,0 +1,23 @@
+package com.skillbox.skillbox.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class OnboardingAdapter(
+    private val screens: List <OnboardingScreen>,
+    activity: FragmentActivity): FragmentStateAdapter(activity) {
+
+
+    override fun getItemCount(): Int {
+        return screens.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val screen: OnboardingScreen = screens [position]
+        return OnboardingFragment.newInstance(
+            screen.textRes,
+            screen.bgColorRes,
+            screen.drawableRes)
+    }
+}
