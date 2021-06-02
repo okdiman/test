891ad3218b31693fca0a8c52a9c74c699ee7effd
Okunev Dmitrii <okdiman@rambler.ru>
2021-06-02 20:37:03 +0500

    go

--- Lists_1/app/src/main/java/com/skillbox/skillbox/myapplication/ListFragment.kt
+++ Lists_1/app/src/main/java/com/skillbox/skillbox/myapplication/ListFragment.kt
@@ -1,56 +1,71 @@
 package com.skillbox.skillbox.myapplication
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.list_fragment.*
 
 class ListFragment() : Fragment(R.layout.list_fragment) {
-    private val resorts = listOf<Resorts>(
-        Resorts.mountains(
-            name = "Aspen, Colorado",
-            country = "USA",
-            photo = R.drawable.aspen,
-            mountain = "Aspen"
-        ),
-        Resorts.oceans(
-            name = "Hawaiian Islands",
-            country = "USA",
-            photo = R.drawable.hawaii,
-            ocean = "Pacific ocean"
-        ),
-        Resorts.mountains(
-            name = "Cortina-d'Ampezzo",
-            country = "Italy",
-            photo = R.drawable.cortina,
-            mountain = "Alps"
-        ),
-        Resorts.oceans(
-            name = "Seychelles islands",
-            country = "Republic of Seychelles",
-            photo = R.drawable.seychelles,
-            ocean = "Indian ocean"
-        ),
-        Resorts.mountains(
-            name = "Mont Tremblant",
-            country = "Canada",
-            photo = R.drawable.mont_tremblant,
-            mountain = "Mont Tremblant"
-        ),
-        Resorts.oceans(
-            name = "Canary islands",
-            country = "Spain",
-            photo = R.drawable.canary,
-            ocean = "Atlantic ocean"
-        ),
-        Resorts.mountains(
-            name = "Chamonix",
-            country = "France",
-            photo = R.drawable.chamonix,
-            mountain = "Alps"
-        ),
-        Resorts.seas(
-            name = "Ibiza",
-            country = "Spain",
-            photo = R.drawable.ibiza,
-            sea = "Mediterranean sea"
-        ),
-    )
+
+    private val resortsList = emptyList<Resorts>()
+//    private val resorts = listOf<Resorts>(
+//        Resorts.Mountains(
+//            name = "Aspen, Colorado",
+//            country = "USA",
+//            photo = R.drawable.aspen,
+//            mountain = "Aspen"
+//        ),
+//        Resorts.Oceans(
+//            name = "Hawaiian Islands",
+//            country = "USA",
+//            photo = R.drawable.hawaii,
+//            ocean = "Pacific ocean"
+//        ),
+//        Resorts.Mountains(
+//            name = "Cortina-d'Ampezzo",
+//            country = "Italy",
+//            photo = R.drawable.cortina,
+//            mountain = "Alps"
+//        ),
+//        Resorts.Oceans(
+//            name = "Seychelles islands",
+//            country = "Republic of Seychelles",
+//            photo = R.drawable.seychelles,
+//            ocean = "Indian ocean"
+//        ),
+//        Resorts.Mountains(
+//            name = "Mont Tremblant",
+//            country = "Canada",
+//            photo = R.drawable.mont_tremblant,
+//            mountain = "Mont Tremblant"
+//        ),
+//        Resorts.Oceans(
+//            name = "Canary islands",
+//            country = "Spain",
+//            photo = R.drawable.canary,
+//            ocean = "Atlantic ocean"
+//        ),
+//        Resorts.Mountains(
+//            name = "Chamonix",
+//            country = "France",
+//            photo = R.drawable.chamonix,
+//            mountain = "Alps"
+//        ),
+//        Resorts.Seas(
+//            name = "Ibiza",
+//            country = "Spain",
+//            photo = R.drawable.ibiza,
+//            sea = "Mediterranean sea"
+//        ),
+//    )
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        addFab.setOnClickListener {
+            addResort()
+        }
+    }
+
+    private fun addResort (){
+
+    }
 }
--- Lists_1/app/src/main/java/com/skillbox/skillbox/myapplication/Resorts.kt
+++ Lists_1/app/src/main/java/com/skillbox/skillbox/myapplication/Resorts.kt
@@ -1,21 +1,21 @@
 package com.skillbox.skillbox.myapplication
 
 sealed class Resorts {
-    data class seas(
+    data class Seas(
         val name: String,
         val country: String,
         val photo: Int,
         val sea: String
     ) : Resorts()
 
-    data class mountains(
+    data class Mountains(
         val name: String,
         val country: String,
         val photo: Int,
         val mountain: String
     ) : Resorts()
 
-    data class oceans(
+    data class Oceans(
         val name: String,
         val country: String,
         val photo: Int,
