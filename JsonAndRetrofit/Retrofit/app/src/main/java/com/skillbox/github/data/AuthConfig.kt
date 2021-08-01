11c3e3c28c54ca337eeed66e148d4d0a041f67e0
Okunev Dmitrii <okdiman@rambler.ru>
2021-08-01 15:02:39 +0500

    go

--- JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
+++ JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
@@ -8,7 +8,6 @@ object AuthConfig {
     const val TOKEN_URI = "https://github.com/login/oauth/access_token"
     const val RESPONSE_TYPE = ResponseTypeValues.CODE
     const val SCOPE = "user,repo"
-
     const val CLIENT_ID = "f4eb5bcce9f94fe6aae0"
     const val CLIENT_SECRET = "868e409ae800f753cb0fd712335ff4bd73e65a78"
     const val CALLBACK_URL = "skillbox://skillbox.ru/callback"
