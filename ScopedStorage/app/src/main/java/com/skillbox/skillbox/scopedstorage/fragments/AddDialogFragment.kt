2adccb8515ac2a90625923e28597a94dc1269e9f
Okunev Dmitrii <okdiman@rambler.ru>
2021-09-10 03:18:33 +0500

    go

---
+++ ScopedStorage/app/src/main/java/com/skillbox/skillbox/scopedstorage/fragments/AddDialogFragment.kt
@@ -0,0 +1,6 @@
+package com.skillbox.skillbox.scopedstorage.fragments
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddDialogFragment: BottomSheetDialogFragment() {
+}
