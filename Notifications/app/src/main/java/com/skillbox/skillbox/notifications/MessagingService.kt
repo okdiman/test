8589d3a9cb5af99c7db6feff7718924111af1c0c
Okunev Dmitrii <okdiman@rambler.ru>
2021-09-25 01:58:29 +0500

    go

--- Notifications/app/src/main/java/com/skillbox/skillbox/notifications/MessagingService.kt
+++ Notifications/app/src/main/java/com/skillbox/skillbox/notifications/MessagingService.kt
@@ -2,6 +2,7 @@ package com.skillbox.skillbox.notifications
 
 
 import android.app.PendingIntent
+import android.app.RemoteInput
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
