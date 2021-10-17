0b05518c4418dc63a2a488ab8081bc1930afcab3
okdiman <87820519+okdiman@users.noreply.github.com>
2021-10-17 20:39:49 +0500

    cart screen

--- TestOnlineShop/app/src/main/java/com/skillbox/skillbox/testonlineshop/presentation/MainActivity.kt
+++ TestOnlineShop/app/src/main/java/com/skillbox/skillbox/testonlineshop/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.skillbox.skillbox.testonlineshop
+package com.skillbox.skillbox.testonlineshop.presentation
 
 import androidx.appcompat.app.AppCompatActivity
+import com.skillbox.skillbox.testonlineshop.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
