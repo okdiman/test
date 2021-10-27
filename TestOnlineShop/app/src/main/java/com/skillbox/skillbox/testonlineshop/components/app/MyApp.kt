5026d556bb474eee29b244c32ad9a70ddf0643f7
okdiman <87820519+okdiman@users.noreply.github.com>
2021-10-27 06:27:43 +0500

    go

--- TestOnlineShop/app/src/main/java/com/skillbox/skillbox/testonlineshop/components/app/MyApp.kt
+++ TestOnlineShop/app/src/main/java/com/skillbox/skillbox/testonlineshop/components/app/MyApp.kt
@@ -1,19 +1,8 @@
 package com.skillbox.skillbox.testonlineshop.components.app
 
 import android.app.Application
-import com.skillbox.skillbox.testonlineshop.R
 import dagger.hilt.android.HiltAndroidApp
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+
 
 @HiltAndroidApp
-class MyApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        CalligraphyConfig.initDefault(
-            CalligraphyConfig.Builder()
-                .setDefaultFontPath("font/markpro.ttf")
-                .setFontAttrId(R.attr.fontPath)
-                .build()
-        )
-    }
-}
+class MyApp : Application()
